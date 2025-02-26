621224e9082c90abedf0d0464a4d822c2dfeb2cf
rockylakshmanan <rockycodepractice@gmail.com>
2025-02-26 12:10:57 +0530

    new pyramid pattern implemented

--- 1_pattern/pattern.cpp
+++ 1_pattern/pattern.cpp
@@ -2,16 +2,28 @@
 
 using namespace std;
 
-/*square_*/
+  /***************************************SQUARE***********************************************/
 
-void square_1(int n)
-{
+/*
+   
+
+  * * *         * * * * *                  
+  * * *         * * * * *
+  * * *         * * * * *
+                * * * * *
+
+
+*/
+
+
+
+void square_1(int n){
     /*
-     n=3
-
-     * * *
-     * * *
-     * * *
+     n=3    row     star space
+ 
+     * * *   1       3    0
+     * * *   2       3    0
+     * * *   3       3    0
 
     */
 
@@ -29,11 +41,11 @@ void square_1(int n)
 void square_2(int n)
 {
     /*
-     n=3
+     n=3       row  number_count
 
-     1 2 3
-     4 5 6
-     7 8 9
+     1 2 3      1       3
+     4 5 6      2       3 
+     7 8 9      3       3
 
     */
     int t=1;
@@ -51,12 +63,14 @@ void square_2(int n)
 
 void square_3(int n)
 {
-    /*
-     n=3
+    /*  
 
-     9 8 7
-     6 5 4
-     3 2 1
+     n=3       row  number_count
+
+     9 8 7      1       3
+     6 5 4      2       3 
+     3 2 1      3       3
+
 
     */
     int t=n*n;
@@ -75,11 +89,14 @@ void square_3(int n)
 void square_4(int n)
 {
     /*
-     n=3
+     
+     n=3       row  number_count
 
-     A B C
-     D E F
-     F G H
+     A B C      1       3
+     D E F      2       3 
+     F G H      3       3
+
+
 
     */
     char ch='A';
@@ -98,11 +115,13 @@ void square_4(int n)
 void square_5(int n)
 {
     /*
-     n=3
+    
+    n=3       row  number_count
 
-     I H G
-     F E D
-     C B A
+     I H G      1       3
+     F E D      2       3 
+     C B A     3       3
+
 
     */
     char ch='A'+((n*n)-1);
@@ -121,11 +140,13 @@ void square_5(int n)
 void square_6(int n)
 {
     /*
-     n=3
+     
+     n=3       row  number_count
 
-     A B A
-     B A B
-     A B A
+     A B A      1       3
+     B A B      2       3 
+     A B A      3       3
+
 
     */
     char ch='A';
@@ -146,12 +167,12 @@ void square_6(int n)
 void square_7(int n)
 {
     /*
-     n=3
 
-     B A B
-     A B A
-     B A B
-     
+    n=3       row  number_count
+
+     B A B      1       3
+     A B A      2       3 
+     B A B      3       3
 
     */
     char ch='A';
@@ -171,7 +192,17 @@ void square_7(int n)
 
 void square_8(int n)
 {
+    
+   /*
+     
+     n=3       row  number_count
 
+     A B C      1       3
+     D E F      2       3 
+     F G H      3       3
+   */
+
+  
     for(int i=1;i<=n;i++)
     {
      
@@ -192,22 +223,78 @@ void square_8(int n)
 
 
 
-/* right_half_pyramid_*/
+
+
+
+ /***************************************PYRAMID***********************************************/
 
 /*
-*
-* *
-* * *
 
-*/
-void right_half_pyramid_1(int n)
-{
-    /*
-     n=3
+    right_half_pyramid
+    ------------------
 
      *
      * *
      * * *
+
+    left_half_pyramid
+    -----------------
+   
+        *
+      * *
+    * * *
+     
+    reverse half pyramid
+    ---------------------
+
+    (i)reverse left
+
+     * * *
+       * *
+         *
+    
+    (ii) reverse right
+    
+     * * *
+     * *
+     * 
+
+    pyramid
+    --------
+ (i)
+
+        *
+      * * *
+    * * * * *
+
+ (ii)
+
+      *
+     * *
+    * * *
+
+   reverse pyramid
+   ----------------
+
+   * * * * *
+     * * *
+       *
+
+
+
+*/
+
+
+/* right_half_pyramid_*/
+
+void right_half_pyramid_1(int n)
+{
+    /*
+     n=3      row    star 
+
+     *         1      1      
+     * *       2      2     
+     * * *     3      3     
      
     */
    
@@ -227,11 +314,12 @@ void right_half_pyramid_1(int n)
 void right_half_pyramid_2(int n)
 {
     /*
-     n=3
+     
+     n=3      row    numb 
 
-     1
-     1 2
-     1 2 3 
+     1         1      1      
+     1 2       2      2     
+     1 2 3     3      3    
         
     */
    
@@ -251,12 +339,13 @@ void right_half_pyramid_2(int n)
 void right_half_pyramid_3(int n)
 {
     /*
-     n=3
+           
+    n=3      row    numb 
 
-     1
-     2 3
-     4 5 6 
-        
+     1         1      1      
+     2 3       2      2     
+     4 5 6     3      3     
+
     */
     int k=1;
     for(int i=1;i<=n;i++)
@@ -276,11 +365,12 @@ void right_half_pyramid_3(int n)
 void right_half_pyramid_4(int n)
 {
     /*
-     n=3
+     
+     n=3      row    numb 
 
-     6
-     5 4
-     3 2 1 
+     6         1      1      
+     5 4       2      2     
+     3 2 1     3      3     
         
     */
     int k=2*n;
@@ -302,11 +392,11 @@ void right_half_pyramid_5(int n)
 {
 
     /*
-     n=3
+     n=3      row    char  
 
-     A
-     A B
-     A B C
+     A         1      1      
+     A B       2      2     
+     A B C     3      3     
 
     */
   for(int i=1;i<=n;i++)
@@ -325,11 +415,12 @@ void right_half_pyramid_6(int n)
 {
 
     /*
-     n=3
+    
+     n=3      row    char  
+     A         1      1      
+     B C       2      2     
+     D E F     3      3     
 
-     A
-     B C
-     D E F
 
     */
    char ch='A';
@@ -349,11 +440,12 @@ void right_half_pyramid_7(int n)
 {
 
     /*
-     n=3
+ 
+     n=3      row    char  
 
-     A
-     A B
-     A B A
+     A         1      1      
+     A B       2      2     
+     A B A     3      3     
 
     */
    
@@ -375,11 +467,13 @@ void right_half_pyramid_8(int n)
 {
 
     /*
-     n=3
+   
+    n=3      row    char  
 
-     A
-     B A 
-     A B A 
+     A         1      1      
+     B A       2      2    
+     A B A     3      3     
+
 
     */
   
@@ -406,12 +500,12 @@ void right_half_pyramid_9(int n)
 {
 
     /*
-     n=3
-
-     1
-     1 0 
-     1 0 1
       
+     n=3      row    char  
+
+     1         1      1      
+     1 0       2      2     
+     1 0 1     3      3     
 
     */
   
@@ -434,12 +528,12 @@ void right_half_pyramid_10(int n)
 {
 
     /*
-     n=3
+         
+     n=3      row    char  
 
-     1
-     0 1  
-     1 0 1
-      
+     1         1      1    
+     0 1       2      2    
+     1 0 1     3      3    
 
     */
   
@@ -459,24 +553,275 @@ void right_half_pyramid_10(int n)
 }
 
 
-
 /* Left Half pyramid*/
 
 void left_half_pyramid1(int n)
 {
 
     /*
-     n=3
-        * 
-      * * 
-    * * * 
-      
+
+     n=3     row    space  star
+        *     1       2     1
+      * *     2       1     2
+    * * *     3       0     3
+   -------------------------------
+                     n-i    i   
      
     */
   
    for(int i=1;i<=n;i++)
   {  
     
+    for(int s=1;s<=n-i;s++)
+    {
+      cout<<"  ";
+    }
+
+    for(int j=1;j<=i;j++)
+    {
+      cout<<"* ";
+    }
+   
+    
+    cout<<endl;
+  }
+
+}
+
+void left_half_pyramid2(int n)
+{
+
+    /*
+
+     n=3     row    space  star
+        1     1       2     1
+      1 2     2       1     2
+    1 2 3     3       0     3
+   -------------------------------
+                     n-i    i   
+     
+    */
+  
+   for(int i=1;i<=n;i++)
+  {  
+    
+    for(int s=1;s<=n-i;s++)
+    {
+      cout<<"  ";
+    }
+
+    for(int j=1;j<=i;j++)
+    {
+      cout<<j<<" ";
+    }
+   
+    
+    cout<<endl;
+  }
+
+}
+
+void left_half_pyramid3(int n)
+{
+
+    /*
+
+     n=3     row    space  star
+        1    1       2     1
+      2 3    2       1     2
+    4 5 6    3       0     3
+   -------------------------------
+                     n-i    i   
+     
+    */
+  int t=1;
+   for(int i=1;i<=n;i++)
+  {  
+    
+    for(int s=1;s<=n-i;s++)
+    {
+      cout<<"  ";
+    }
+
+    for(int j=1;j<=i;j++)
+    {
+      cout<<t<<" ";
+      t++;
+    }
+   
+    
+    cout<<endl;
+  }
+
+}
+
+void left_half_pyramid4(int n)
+{
+
+    /*
+
+     n=3     row    space  star
+        A    1       2     1
+      A B    2       1     2
+    A B C    3       0     3
+   -------------------------------
+                     n-i    i   
+     
+    */
+ // int t=1;
+   for(int i=1;i<=n;i++)
+  {  
+    char ch='A';
+    for(int s=1;s<=n-i;s++)
+    {
+      cout<<"  ";
+    }
+
+    for(int j=1;j<=i;j++)
+    {
+      cout<<ch<<" ";
+      ch++;
+    }
+   
+    
+    cout<<endl;
+  }
+
+}
+
+void left_half_pyramid5(int n)
+{
+
+    /*
+
+     n=3     row    space  star
+        A    1       2     1
+      B C    2       1     2
+    D E F    3       0     3
+   -------------------------------
+                     n-i    i   
+     
+    */
+ // int t=1;
+ char ch='A';
+   for(int i=1;i<=n;i++)
+  {  
+    
+    for(int s=1;s<=n-i;s++)
+    {
+      cout<<"  ";
+    }
+
+    for(int j=1;j<=i;j++)
+    {
+      cout<<ch<<" ";
+      ch++;
+    }
+   
+    
+    cout<<endl;
+  }
+
+}
+
+void left_half_pyramid6(int n)
+{
+
+    /*
+
+     n=3     row    space  star
+        C    1       2     1
+      C B    2       1     2
+    C B A    3       0     3
+   -------------------------------
+                     n-i    i   
+     
+    */
+ // int t=1;
+ 
+   for(int i=1;i<=n;i++)
+  {  
+    char ch='A'+n-1;
+    for(int s=1;s<=n-i;s++)
+    {
+      cout<<"  ";
+    }
+
+    for(int j=1;j<=i;j++)
+    {
+      cout<<ch<<" ";
+      ch--;
+    }
+   
+    
+    cout<<endl;
+  }
+
+}
+
+void left_half_pyramid7(int n)
+{
+
+    /*
+
+     n=3     row    space  star
+        A    1       2     1
+      B A     2       1     2
+    A B A    3       0     3
+   -------------------------------
+                     n-i    i   
+     
+    */
+ // int t=1;
+ 
+   for(int i=1;i<=n;i++)
+  {  
+    char ch= (i%2!=0)?'A':'B';
+    for(int s=1;s<=n-i;s++)
+    {
+      cout<<"  ";
+    }
+
+    for(int j=1;j<=i;j++)
+    {
+      cout<<ch<<" ";
+      ch=(ch=='A')?'B':'A';
+    }
+   
+    
+    cout<<endl;
+  }
+
+}
+
+void left_half_pyramid8(int n)
+{
+
+    /*
+
+     n=3     row    space  star
+        A    1       2     1
+      A B    2       1     2
+    A B A    3       0     3
+   -------------------------------
+                     n-i    i   
+     
+    */
+ // int t=1;
+ 
+   for(int i=1;i<=n;i++)
+  {  
+    char ch= 'A';
+    for(int s=1;s<=n-i;s++)
+    {
+      cout<<"  ";
+    }
+
+    for(int j=1;j<=i;j++)
+    {
+      cout<<ch<<" ";
+      ch=(ch=='A')?'B':'A';
+    }
    
     
     cout<<endl;
@@ -500,10 +845,10 @@ void rev_half_pyramid_1(int n)
 {
 
     /*
-     n=3
-     * * *
-     * *
-     *           
+     n=3     row   star   
+     * * *   1      3      
+     * *     2      2
+     *       3      1
 
     */
   
@@ -525,13 +870,14 @@ void rev_half_pyramid_1(int n)
 void rev_half_pyramid_2(int n)
 {
 
-    /*
-     n=3
-     1 1 1
-     2 2
-     3           
-
-    */
+    /* 
+                  count
+     n=3    row   number
+     1 1 1   1      3
+     2 2     2      2
+     3       3      1
+     
+     */
   
   for(int i=1;i<=n;i++)
   {  
@@ -551,11 +897,12 @@ void rev_half_pyramid_3(int n)
 {
 
     /*
-     n=3
-     1 2 3
-     1 2
-     1           
-
+        
+                  count
+     n=3    row   number
+     1 2 3   1      3
+     1 2     2      2
+     1       3      1
     */
   
   for(int i=1;i<=n;i++)
@@ -576,10 +923,11 @@ void rev_half_pyramid_4(int n)
 {
 
     /*
-     n=3
-     1 2 3
-     4 5
-     6           
+                  count
+     n=3    row   number
+     1 2 3   1      3
+     4 5     2      2
+     6       3      1
 
     */
   int temp=1;
@@ -601,11 +949,11 @@ void rev_half_pyramid_5(int n)
 {
 
     /*
-     n=3
-     
-     A B C
-     A B 
-     A
+                       
+     n=3    row   char
+     A B C   1      3
+     A B     2      2
+     A       3      1
 
     */
   
@@ -627,12 +975,13 @@ void rev_half_pyramid_6(int n)
 {
 
     /*
-     n=3
-     
-     C B A
-     C B 
-     C
+          
+     n=3    row   char
+     C B A   1      3
+     C B     2      2
+     C       3      1
 
+     
     */
   
    for(int i=1;i<=n;i++)
@@ -655,11 +1004,12 @@ void rev_half_pyramid_7(int n)
 {
 
     /*
-     n=3
      
-     1 0 1
-     1 0
-     1
+     n=3    row    num count
+     1 0 1   1      3
+     1 0     2      2
+     1       3      1
+
 
     */
   
@@ -682,11 +1032,11 @@ void rev_half_pyramid_8(int n)
 {
 
     /*
-     n=3
      
-     1 0 1
-     0 1
-     1
+     n=3    row    num count
+     1 0 1   1      3
+     0 1     2      2
+     1       3      1
 
     */
   
@@ -709,12 +1059,11 @@ void rev_half_pyramid_9(int n)
 {
 
     /*
-     n=3
-     
-     A B A
-     A B
-     A
-     
+          
+     n=3    row    num count
+     A B A   1      3
+     A B     2      2
+     A       3      1
 
     */
   
@@ -737,12 +1086,13 @@ void rev_half_pyramid_10(int n)
 {
 
     /*
-     n=3
-     
-     A B A
-     B A
-     A
-     
+        
+     n=3    row   char_count
+     A B A   1      3
+     B A     2      2
+     A       3      1
+
+
     */
   
    for(int i=1;i<=n;i++)
@@ -941,6 +1291,16 @@ int main()
         //right_half_pyramid_9(i);
         //right_half_pyramid_10(i);
 
+        //left_half_pyramid1(i);
+        //left_half_pyramid2(i);
+        //left_half_pyramid3(i);
+        //left_half_pyramid4(i);
+        // left_half_pyramid5(i);
+        //left_half_pyramid6(i);
+        //left_half_pyramid7(i);
+        left_half_pyramid8(i);
+
+          
         
 
 
@@ -958,7 +1318,9 @@ int main()
 
         //pyramid_1b(i);
         //pyramid_2(i);
-          pyramid_3(i);
+        //  pyramid_3(i);
+
+        
         cout<<"-----------------x----------------\n";
     }
 
